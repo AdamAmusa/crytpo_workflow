058e6982f233625b32e8f284dae1b7afca2857c1
AdamAmusa <92576185+AdamAmusa@users.noreply.github.com>
2024-11-26 20:10:37 +0000

    Crypto Currencies are displayed on home screen

--- crypto-app/src/app/home/home.page.ts
+++ crypto-app/src/app/home/home.page.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+import { NoopAnimationPlayer } from '@angular/animations';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList } from '@ionic/angular/standalone';
+import { NgFor } from '@angular/common';
+import { CrpytoService } from '../services/crpyto.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, NgFor, IonList, MatPaginatorModule],
 })
+
+
 export class HomePage {
-  constructor() {}
+
+ cryptos: any[] = [];
+ displayedCryptos: any[] = [];
+ pageSize = 15;
+length = 0;
+pageIndex = 0;
+
+  constructor(private crypto:CrpytoService) {}
+
+  async ngOnInit() {
+   const data = await this.crypto.getCoinList();
+    this.cryptos = data;
+    this.length = data.length;
+    this.updateDisplayedCryptos();
+  }
+
+  updateDisplayedCryptos() {
+    const startIndex = this.pageIndex * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.displayedCryptos = this.cryptos.slice(startIndex, endIndex);
+  }
+
+  onPageChange(event: PageEvent) {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.updateDisplayedCryptos();
+  }
+  
 }
--- crypto-app/src/app/services/crpyto.service.ts
+++ crypto-app/src/app/services/crpyto.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
 
+
+
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class CrpytoService {
+private readonly url = "https://api.coingecko.com/api/v3/coins/list";
+private readonly options = {method: 'GET', headers: {accept: 'application/json'}};
+
+
+
 
   constructor() { }
+
+  async getCoinList(): Promise<any> {   
+    try {
+      const response = await fetch(this.url, this.options);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching coin list:', error);
+      return [];
+    }
+  }
 }
