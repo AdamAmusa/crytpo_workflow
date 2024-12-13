fc20ba12aecedb43a14aed88100a031b21c39e2c
AdamAmusa <92576185+AdamAmusa@users.noreply.github.com>
2024-12-13 21:48:40 +0000

    Installed firebase

--- crypto-app/src/main.ts
+++ crypto-app/src/main.ts
@@ -4,13 +4,45 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-        provideAnimations()
+    provideAnimations(), provideFirebaseApp(() => initializeApp
+      ({
+        "projectId": "crypto-be9d5",
+        "appId": "1:926217799510:web:5d2151806cbcb9f3ab38f7",
+        "storageBucket": "crypto-be9d5.firebasestorage.app",
+        "apiKey": "AIzaSyCs4PnUAFt8EUkhthZsqYJuaedq9hJJiRA",
+        "authDomain": "crypto-be9d5.firebaseapp.com",
+        "messagingSenderId": "926217799510",
+        "measurementId": "G-29WZZ9K3DX"
+      })),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService, UserTrackingService,
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideVertexAI(() => getVertexAI())
 
   ],
 });
